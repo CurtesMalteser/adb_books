0cf3941fbeace8dc420c3e311369195cfad0acbb
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-09-17 21:40:49 +0200

    feat: enable Auth0 JWT validation
    - Test with Auth0 sample app
    - Test with cURL
    - Test with Postman

--- backend/app/auth/auth.py
+++ backend/app/auth/auth.py
@@ -1,26 +1,27 @@
-from os import environ as env
-# import json
-from flask import request
+import json
 from functools import wraps
-from jose import jwt
-# from urllib.request import urlopen
+from os import environ as env
+from urllib.request import urlopen
+
 from dotenv import load_dotenv, find_dotenv
+from flask import request
+from jose import jwt
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(ENV_FILE)
 
-
 AUTH0_DOMAIN = env.get('AUTH0_DOMAIN')
 API_AUDIENCE = env.get('API_AUDIENCE')
 ALGORITHMS = ['RS256']
 
-## AuthError Exception
-"""
-AuthError Exception
-A standardized way to communicate auth failure modes
-"""
+
 class AuthError(Exception):
+    """
+    AuthError Exception
+    A standardized way to communicate auth failure modes
+    """
+
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
@@ -44,14 +45,13 @@ def get_token_auth_header():
             'description': 'Authorization header is expected.'
         }, 401)
 
-
     parts = auth.split()
 
     if parts[0].lower() != 'bearer':
         raise AuthError({
             'code': 'invalid_header',
             'description': 'Authorization header must start with "Bearer".'
-            }, 401)
+        }, 401)
 
     if len(parts) == 1:
         raise AuthError({
@@ -67,6 +67,7 @@ def get_token_auth_header():
 
     token = parts[1]
     return token
+
 
 def check_permissions(permission, payload):
     """
@@ -95,82 +96,75 @@ def check_permissions(permission, payload):
 
     return True
 
-# @TODO uncomment to validate actual Auth0
-# currently testing with dummy from JWT.io
-# def verify_decode_jwt(token):
-#     """
-#     @TODO implement verify_decode_jwt(token) method
-#         @INPUTS
-#             token: a json web token (string)
-#
-#         it should be an Auth0 token with key id (kid)
-#         it should verify the token using Auth0 /.well-known/jwks.json
-#         it should decode the payload from the token
-#         it should validate the claims
-#         return the decoded payload
-#
-#         !!NOTE urlopen has a common certificate error described here:
-#         https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
-#     """
-#     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
-#     jwks = json.loads(jsonurl.read())
-#     unverified_header = jwt.get_unverified_header(token)
-#     rsa_key = {}
-#     if 'kid' not in unverified_header:
-#         raise AuthError({
-#             'code': 'invalid_header',
-#             'description': 'Authorization malformed.'
-#         }, 401)
-#
-#     for key in jwks['keys']:
-#         if key['kid'] == unverified_header['kid']:
-#             rsa_key = {
-#                 'kty': key['kty'],
-#                 'kid': key['kid'],
-#                 'use': key['use'],
-#                 'n': key['n'],
-#                 'e': key['e']
-#             }
-#     if rsa_key:
-#         try:
-#             payload = jwt.decode(
-#                 token,
-#                 rsa_key,
-#                 algorithms=ALGORITHMS,
-#                 audience=API_AUDIENCE,
-#                 issuer='https://' + AUTH0_DOMAIN + '/'
-#             )
-#
-#             return payload
-#
-#         except jwt.ExpiredSignatureError as e:
-#             raise AuthError({
-#                 'code': 'token_expired',
-#                 'description': 'Token expired.'
-#             }, 401) from e
-#
-#         except jwt.JWTClaimsError as e:
-#             raise AuthError({
-#                 'code': 'invalid_claims',
-#                 'description': 'Incorrect claims. Please, check the audience and issuer.'
-#             }, 401) from e
-#
-#         except Exception as e:
-#             raise AuthError({
-#                 'code': 'invalid_header',
-#                 'description': 'Unable to parse authentication token.'
-#             }, 400) from e
-#
-#     raise AuthError({
-#                 'code': 'invalid_header',
-#                 'description': 'Unable to find the appropriate key.'
-#             }, 400)
 
-# @TODO: replace with method above to handle AuthO
-ALGORITHM = 'HS256' #HMAC-SHA 256
-SECRET = 'your-256-bit-secret'
 def verify_decode_jwt(token):
-    return jwt.decode(token, SECRET, algorithms=[ALGORITHM])
+    """
+    @TODO implement verify_decode_jwt(token) method
+        @INPUTS
+            token: a json web token (string)
+
+        it should be an Auth0 token with key id (kid)
+        it should verify the token using Auth0 /.well-known/jwks.json
+        it should decode the payload from the token
+        it should validate the claims
+        return the decoded payload
+
+        !!NOTE urlopen has a common certificate error described here:
+        https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
+    """
+    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+    if 'kid' not in unverified_header:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
+
+            return payload
+
+        except jwt.ExpiredSignatureError as e:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token expired.'
+            }, 401) from e
+
+        except jwt.JWTClaimsError as e:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
+            }, 401) from e
+
+        except Exception as e:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 400) from e
+
+    raise AuthError({
+        'code': 'invalid_header',
+        'description': 'Unable to find the appropriate key.'
+    }, 400)
 
 
 def requires_auth(permission=''):
@@ -185,6 +179,7 @@ def requires_auth(permission=''):
         permission
         return the decorator which passes the decoded payload to the decorated method
     """
+
     def requires_auth_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
@@ -194,4 +189,5 @@ def requires_auth(permission=''):
             return f(payload, *args, **kwargs)
 
         return wrapper
+
     return requires_auth_decorator
