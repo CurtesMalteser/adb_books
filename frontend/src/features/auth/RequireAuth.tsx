4132daef32f2b57522c1d8b10c563af34b924734
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-09-28 17:55:37 +0200

    feat: Fix RequireAuth always performing redirect to login
    - Add loading state to wait for actual isAuthenticated state

--- frontend/src/features/auth/RequireAuth.tsx
+++ frontend/src/features/auth/RequireAuth.tsx
@@ -1,24 +1,18 @@
-import { useEffect } from 'react';
 import {
   Navigate,
   useLocation,
-  useNavigate,
 } from 'react-router-dom';
 import { useAuth0 } from "@auth0/auth0-react";
 import ROUTES from '../../constants/Routes';
 
 function RequireAuth({ children }: { children: React.ReactNode }) {
 
-  const { isAuthenticated } = useAuth0();
-
-  const navigate = useNavigate();
+  const { isLoading, isAuthenticated } = useAuth0();
   const location = useLocation();
 
-  useEffect(() => {
-    if (!isAuthenticated) {
-      navigate(ROUTES.LOGIN, { state: { from: location } });
-    }
-  }, [isAuthenticated, navigate, location]);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   if (!isAuthenticated) {
     return <Navigate to={ROUTES.LOGIN} replace state={{ path: location }} />
