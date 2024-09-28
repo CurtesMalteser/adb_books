c5327566b617856630672aaf03bb9cc97d7abfb6
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-09-28 18:16:25 +0200

    feat: Fix returnTo by leveraging on useLocation to return to the path that tiggers login

--- frontend/src/features/auth/LoginButton.tsx
+++ frontend/src/features/auth/LoginButton.tsx
@@ -1,14 +1,18 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "react-bootstrap";
 import ROUTES from "../../constants/Routes";
+import { useLocation } from "react-router-dom";
 
 const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
+    const location = useLocation();
 
     const handleSignUp = async () => {
+      const returnTo = location.state?.path || ROUTES.HOME;
+
       await loginWithRedirect({
         appState: {
-          returnTo: ROUTES.MY_BOOKLIST,
+          returnTo: returnTo,
         },
         authorizationParams: {
           screen_hint: "signup",
